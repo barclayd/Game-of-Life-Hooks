722cfbb0cb01e0417055063ca10a33827659cf78
Daniel Barclay <barclaysd@me.com>
2019-11-15 21:33:14 +0000

    Clean up repo and track simulation running state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import produce from 'immer';
 
 const numRows = 50;
 const numCols = 50;
@@ -13,27 +13,40 @@ const App: React.FC = () => {
     return rows;
   });
 
+  const [running, setRunning] = useState(false);
+
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 20px)`,
-      }}
-    >
-      {grid.map((rows, rowIdx) =>
-        rows.map((col, colIdx) => (
-          <div
-            key={`${rowIdx}-${colIdx}`}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[rowIdx][colIdx] ? '#003366' : '#eee',
-              border: '1px solid black',
-            }}
-          />
-        )),
-      )}
-    </div>
+    <>
+      <button onClick={() => setRunning(!running)}>
+        {!running ? 'Start' : 'Stop'}
+      </button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, rowIdx) =>
+          rows.map((col, colIdx) => (
+            <div
+              key={`${rowIdx}-${colIdx}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[rowIdx][colIdx] = grid[rowIdx][colIdx] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[rowIdx][colIdx] ? '#003366' : '#eee',
+                border: '1px solid black',
+              }}
+            />
+          )),
+        )}
+      </div>
+    </>
   );
 };
 
