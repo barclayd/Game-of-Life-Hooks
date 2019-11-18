f5cb2d4d1b776383734c1f31d5c3454a8457b026
Daniel Barclay <barclaysd@me.com>
2019-11-18 20:33:28 +0000

    Updates

---
+++ precache-manifest.240d102c7dbf6618bc823392eb4ba8ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae08a5c7eb62d83c15e8f2648721fd50",
+    "url": "/game-of-life-hooks/index.html"
+  },
+  {
+    "revision": "9c0e372e563af044ea75",
+    "url": "/game-of-life-hooks/static/js/2.6079cf2c.chunk.js"
+  },
+  {
+    "revision": "c8136407e2b70bedac9e",
+    "url": "/game-of-life-hooks/static/js/main.8e12b2a3.chunk.js"
+  },
+  {
+    "revision": "0bcb888b86affa13a8ad",
+    "url": "/game-of-life-hooks/static/js/runtime-main.50388ff1.js"
+  }
+]);
